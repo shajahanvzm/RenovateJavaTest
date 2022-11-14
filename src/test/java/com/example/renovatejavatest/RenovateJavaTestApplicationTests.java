3ce1dfa13a6a0d4fafec63f179d771df10344074
shajahanvzm <shajahanvzm@gmail.com>
2022-11-14 17:44:11 +0530

    first commit'

---
+++ src/test/java/com/example/renovatejavatest/RenovateJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renovatejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
